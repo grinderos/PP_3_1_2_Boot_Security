2f008d86a8822cbcbe8478f7bc360d905e1d140d
grinderos <grinderos.coding@gmail.com>
2024-04-18 15:49:42 +0300

    3_1_3 security commit before alishev lesson 80

--- src/main/java/ru/kata/spring/boot_security/security/AuthProviderImpl.java
+++ src/main/java/ru/kata/spring/boot_security/security/AuthProviderImpl.java
@@ -1,48 +1,45 @@
-//package ru.kata.spring.boot_security.security;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.authentication.AuthenticationProvider;
-//import org.springframework.security.authentication.BadCredentialsException;
-//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-//import org.springframework.security.core.Authentication;
-//import org.springframework.security.core.AuthenticationException;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.stereotype.Component;
-//import ru.kata.spring.boot_security.service.LoggedUserDetailsService;
-//
-//import java.util.Collections;
-//
-//@Component
-//public class AuthProviderImpl implements AuthenticationProvider {
-//
-//    private final LoggedUserDetailsService loggedUserDetailsService;
-//
-//    @Autowired
-//    public AuthProviderImpl(LoggedUserDetailsService loggedUserDetailsService) {
-//        this.loggedUserDetailsService = loggedUserDetailsService;
-//    }
-//
-//    @Override
-//    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-//        String username = authentication.getName();
-//        UserDetails userDetails = loggedUserDetailsService.loadUserByUsername(username);
-//        String password = authentication.getCredentials().toString();
-//
-//        if(!password.equals(userDetails.getPassword())) {
-//            throw new BadCredentialsException("неверный пароль");
-//        }
-//
-//        return new UsernamePasswordAuthenticationToken(userDetails, password,
-////                userDetails.getAuthorities());
-//                Collections.emptyList());
-//    }
-//
-//    @Override
-//    public boolean supports(Class<?> authentication) {
-////        return false;
-//        return true;
-//    }
-//}
+package ru.kata.spring.boot_security.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import ru.kata.spring.boot_security.service.LoggedUserDetailsService;
 
-//убираем, потому что реализовали стандартные интерфейсы для работы с принципл и юзерДетаилс
+import java.util.Collections;
+
+@Component
+public class AuthProviderImpl implements AuthenticationProvider {
+
+    private final LoggedUserDetailsService loggedUserDetailsService;
+
+    @Autowired
+    public AuthProviderImpl(LoggedUserDetailsService loggedUserDetailsService) {
+        this.loggedUserDetailsService = loggedUserDetailsService;
+    }
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        UserDetails userDetails = loggedUserDetailsService.loadUserByUsername(username);
+        String password = authentication.getCredentials().toString();
+
+        if(!password.equals(userDetails.getPassword())) {
+            throw new BadCredentialsException("неверный пароль");
+        }
+
+        return new UsernamePasswordAuthenticationToken(userDetails, password,
+//                userDetails.getAuthorities());
+                Collections.emptyList());
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+//        return false;
+        return true;
+    }
+}
