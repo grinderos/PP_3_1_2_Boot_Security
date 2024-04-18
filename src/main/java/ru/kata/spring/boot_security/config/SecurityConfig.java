d1a3d14941447a0000b52af83e3399e6b6297d93
grinderos <grinderos.coding@gmail.com>
2024-04-18 16:30:30 +0300

    3_1_3 security commit alishev lesson 81

--- src/main/java/ru/kata/spring/boot_security/config/SecurityConfig.java
+++ src/main/java/ru/kata/spring/boot_security/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package ru.kata.spring.boot_security.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -22,6 +23,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     //настраивает аутентификацию
         protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(loggedUserDetailsService);
+    }
+
+    //конфигурируем сам спринг секьюрити и авторизацию
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                //отключаем защиту от межсайтовой подделки запросов
+                .csrf().disable()
+                //правила авторизации. Матчеры срабатывают последовательно (как при исключениях), что означает
+                //применение при первом подходящем совпадении.
+                .authorizeRequests()
+                .antMatchers("/auth/login", "/error")
+                .permitAll()
+                .anyRequest().authenticated()
+
+                .and()
+
+                .formLogin()
+                .loginPage("/auth/login")
+                .loginProcessingUrl("/process_login")
+                .defaultSuccessUrl("/hello", true)
+                .failureUrl("/auth/login?error");
     }
 
     @Bean
